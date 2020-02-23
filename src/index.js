e6f8ffaf527d14e9ee4d94ec123bbea3fda072de
putrann <bichphuong0452@gmail.com>
2020-02-23 12:14:22 +0700

    add actions

--- src/index.js
+++ src/index.js
@@ -1,46 +1,8 @@
-import C from './constants';
 import storeFactory from './store';
+import { addDay, removeDay, setGoal } from './store/actions';
 
-const initialState = localStorage['redux-store']
-  ? JSON.parse(localStorage['redux-store'])
-  : {};
+const store = storeFactory();
 
-const saveState = () => {
-  const state = JSON.stringify(store.getState());
-  console.log('object');
-  localStorage['redux-store'] = state;
-};
-
-const store = storeFactory(initialState);
-
-store.subscribe(saveState);
-
-store.dispatch({
-  type: C.ADD_DAY,
-  payload: {
-    resort: 'Mt Shasta',
-    date: '2016-10-28',
-    powder: false,
-    backcountry: true,
-  },
-});
-
-store.dispatch({
-  type: C.ADD_DAY,
-  payload: {
-    resort: 'Mt Pu',
-    date: '2016-4-28',
-    powder: true,
-    backcountry: true,
-  },
-});
-
-store.dispatch({
-  type: C.ADD_DAY,
-  payload: {
-    resort: 'Mt nguyen',
-    date: '2016-10-8',
-    powder: true,
-    backcountry: false,
-  },
-});
+store.dispatch(addDay('Ms pu', 'Snowbird'));
+store.dispatch(removeDay('23-11-2013'));
+store.dispatch(setGoal(8));
